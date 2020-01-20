1d74ab40f6c19289dd7562fb4dab6bc2de1882a0
n-hau <n-hau@ruby-dev.jp>
2020-01-20 11:36:29 +0700

    Up

--- .ebextensions/hooks/sqlite3.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd ~/
-wget https://www.sqlite.org/2019/sqlite-autoconf-3300100.tar.gz
-tar zxvf sqlite-autoconf-3300100.tar.gz
-cd sqlite-autoconf-3300100
-./configure --prefix=/usr
-make
-make install
