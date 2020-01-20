59e167ecc78a521ed9d8cc49c4f53959a246d800
n-hau <n-hau@ruby-dev.jp>
2020-01-20 10:44:15 +0700

    Change path

--- .ebextensions/hooks/sqlite3.sh
+++ .ebextensions/hooks/sqlite3.sh
@@ -4,6 +4,6 @@ cd ~/
 wget https://www.sqlite.org/2019/sqlite-autoconf-3300100.tar.gz
 tar zxvf sqlite-autoconf-3300100.tar.gz
 cd sqlite-autoconf-3300100
-./configure --prefix=/usr/local
+./configure --prefix=/usr
 make
-sudo make install
+make install
